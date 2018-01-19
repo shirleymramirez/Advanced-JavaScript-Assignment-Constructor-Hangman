1026e3c8067705b5003abe1bd27d5149b9c265db
jheilhet <shirley.mramirez@yahoo.com>
2018-01-18 21:02:26 -0700

    created constructor function for letter.js  and word.js files

--- letter.js
+++ letter.js
@@ -1,4 +1,19 @@
 function Letter(letChar) {
     this.letChar = letChar;
+    // this will check the boolean/property can be shown
+    this.appear = false;
+
+    this.letterRender = function() {
+        // render a blank 
+        if (this.letChar == '') {
+            this.appear = true;
+            return ' ';
+        }
+        if (this.appear === false) {
+            return '_';
+        } else {
+            return this.letChar;
+        }
+    }
 }
 module.exports = Letter;
--- word.js
+++ word.js
@@ -2,6 +2,33 @@ var Letter = require('./letter.js');
 
 var Word = function(charWord) {
     this.charWord = charWord;
+
+    // will hold all the letters
+    this.letChar = [];
+
+    this.found = false;
+
+    // get letter function
+    this.getLetter = function() {
+        for (var i = 0; i < this.charWord.length; i++) {
+            this.letChar.push(new Letter(this.charWord[i]));
+        }
+    }
+
+    // check word function
+    this.foundWord = function() {
+        this.found = this.letChar.every(function(currentLetter) {
+            return currentLetter.appear;
+        })
+        this.found = true;
+        return true;
+    }
+
+    // check if letter found
+    this.checkLetterFound = function(guessedLetter) {
+
+    }
+
 }
 
 module.exports = Word;
